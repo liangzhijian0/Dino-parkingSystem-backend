4da923ffe2d30b463045eeed307c3ed167e10b1a
“zojian” <hellozojian@gmail.com>
2018-07-31 00:20:52 +0800

    init

---
+++ src/main/java/com/oocl/dino_parking_system/DinoParkingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.oocl.dino_parking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinoParkingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinoParkingSystemApplication.class, args);
+    }
+}
